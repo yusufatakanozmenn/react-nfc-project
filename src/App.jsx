78792d6dfd156f77ba6b302ebbda024ce6b6c8bf
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2026-09-22 11:24:26 +0300

    vite kuruldu

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  //NFC projesi
+  return (
+    <div className="App">
+      <h1>NFC Yonetim Paneli</h1>
+    </div>
+  );
+}
+
+export default App;
