29657772f960b65f51602fde89f510a1268c16b4
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2026-09-23 16:12:39 +0300

    servis yapısı kuruldu.

--- backend/src/main/java/com/webonix/webonix_tap_backend/controller/NfcCardController.java
+++ backend/src/main/java/com/webonix/webonix_tap_backend/controller/NfcCardController.java
@@ -2,45 +2,25 @@ package com.webonix.webonix_tap_backend.controller;
 
 import com.webonix.webonix_tap_backend.dto.CreateNfcCardRequest;
 import com.webonix.webonix_tap_backend.dto.NfcCardResponse;
+import com.webonix.webonix_tap_backend.service.NfcCardService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/cards")
 public class NfcCardController {
 
-    private final List<NfcCardResponse> cards = new ArrayList<>();
+    private final NfcCardService nfcCardService;
 
-    public NfcCardController() {
-
-        cards.add(new NfcCardResponse(
-                1L,
-                "Google Yorum Kartı",
-                "google",
-                "A8K2XP",
-                "https://google.com",
-                128,
-                true
-        ));
-
-        cards.add(new NfcCardResponse(
-                2L,
-                "Instagram Kartı",
-                "instagram",
-                "B7X92M",
-                "https://instagram.com",
-                54,
-                true
-        ));
+    public NfcCardController(NfcCardService nfcCardService) {
+        this.nfcCardService = nfcCardService;
     }
 
     @GetMapping
     public List<NfcCardResponse> getCards() {
-        return cards;
+        return nfcCardService.getAllCards();
     }
 
     @PostMapping
@@ -48,27 +28,6 @@ public class NfcCardController {
     public NfcCardResponse createCard(
             @RequestBody CreateNfcCardRequest request
     ) {
-
-        String code = UUID.randomUUID()
-                .toString()
-                .replace("-", "")
-                .substring(0, 6)
-                .toUpperCase();
-
-        Long id = (long) (cards.size() + 1);
-
-        NfcCardResponse newCard = new NfcCardResponse(
-                id,
-                request.name(),
-                request.type(),
-                code,
-                request.destinationUrl(),
-                0,
-                true
-        );
-
-        cards.add(newCard);
-
-        return newCard;
+        return nfcCardService.createCard(request);
     }
 }
